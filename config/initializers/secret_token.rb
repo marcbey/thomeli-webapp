a9e79fc7abb0b5cbc096969e9c65744584a1c308
Marc Beyerlin <marc@beyerlin.net>
2014-01-24 15:49:21 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thomeli2Webapp::Application.config.secret_token = '0e9b108594118ffa1a4f2404a7f43e8f2e8f4a12e62c9107bbf8db41ce08af2371a8f83b15440586f77ac910b8529eaf433d04186a03b9830bca5de692e57627'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Thomeli2Webapp::Application.config.session_store :cookie_store, key: '_thomeli-2-webapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Thomeli2Webapp::Application.config.session_store :active_record_store
