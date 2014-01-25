9b6e8e8c645d72c829aecca4ebf42389acc3bcac
Marc Beyerlin <marc@beyerlin.net>
2014-01-25 01:24:57 +0100

    Add sqlite db as a shared resource

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/home/rails'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{db/production.sqlite3}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{photos bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
