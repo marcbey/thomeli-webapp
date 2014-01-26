73a2b67f831f4e3ea111f17b202ab7f2b8d60b3d
Marc Schopper <ms@fournova.com>
2014-01-26 22:28:39 +0100

    Add sending photo inline

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,8 @@ class PhotosController < ApplicationController
   def download
     photo_manager = PhotoManager.photo_with_token( params[:token] )
 
-    send_data photo_manager.photo, filename: photo_manager.photo_name
+    disposition = params[:disposition] || 'attachment'
+
+    send_data photo_manager.photo, filename: photo_manager.photo_name, disposition: disposition
   end
 end
