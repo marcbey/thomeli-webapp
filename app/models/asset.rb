92e995e952c4a9f39a755acfe303a236a7720557
Marc Schopper <ms@fournova.com>
2014-02-01 01:03:15 +0100

    Fix naming

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,6 @@
 class Asset < ActiveRecord::Base
-  dragonfly_accessor :qrcode_image
-  dragonfly_accessor :photo_image
+  dragonfly_accessor :qrcode
+  dragonfly_accessor :photo
 
   def self.recent( limit = 10 )
     where( 'created_at > ?', 10.days.ago ).limit( limit )
