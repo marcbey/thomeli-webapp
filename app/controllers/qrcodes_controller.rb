591648d216e9431ccf3f2e5ad04f1b9781bf2b75
Marc Schopper <ms@fournova.com>
2014-02-01 01:06:32 +0100

    Fix qrcode controller 

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -2,11 +2,15 @@ class QrcodesController < ApplicationController
   def generate
     photo_name = params[:photo_name]
 
-    token_manager = TokenManager.new
-    qrcode_manager = QrcodeManager.new( token_manager.token, photo_name )
-    # photo_manager = PhotoManager.new( photo_name )
-    # asset_manager = AssetManager.new( qrcode_manager.qrcode_image, photo_manager.photo_image, photo_name )
-    # asset_manager.save!
+    qrcode_manager = QrcodeManager.new( TokenManager, photo_name )
+    token = qrcode_manager.token
+
+    photo_manager = PhotoManager.new( photo_name )
+    asset_manager = AssetManager.new( qrcode_manager.qrcode_image, 
+                                      photo_manager.photo_image, 
+                                      photo_name, token,
+                                      qrcode_manager.url )
+    asset_manager.save!
 
     send_data qrcode_manager.qrcode_image,
               filename: 'qrcode.png',
