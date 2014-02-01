3d4dd4293f012d5e1ae11729e36094feb793917a
Marc Schopper <ms@fournova.com>
2014-02-01 01:02:13 +0100

    Remove unneeded columns 

--- db/migrate/20140131071942_add_columns_for_photo.rb
+++ db/migrate/20140131071942_add_columns_for_photo.rb
@@ -2,9 +2,6 @@ class AddColumnsForPhoto < ActiveRecord::Migration
   def up
     add_column :assets, :photo_uid, :string
     add_column :assets, :photo_size, :integer
-    add_column :assets, :photo_mime, :string
-    add_column :assets, :photo_height, :integer
-    add_column :assets, :photo_width, :intger
 
     remove_column :assets, :imagepath
   end
@@ -14,9 +11,6 @@ class AddColumnsForPhoto < ActiveRecord::Migration
 
     remove_column :assets, :photo_uid
     remove_column :assets, :photo_size
-    remove_column :assets, :photo_mime
-    remove_column :assets, :photo_height
-    remove_column :assets, :photo_width
   end
 end
 
