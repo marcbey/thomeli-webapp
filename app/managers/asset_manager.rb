681f051885cce0a4dc22e24de48cbc9570f8b56a
Marc Schopper <ms@fournova.com>
2014-02-02 19:41:09 +0100

    refactor to @assert instance variable  

--- app/managers/asset_manager.rb
+++ app/managers/asset_manager.rb
@@ -1,5 +1,5 @@
 class AssetManager
-  attr_reader :qrcode_manager, :photo_manager, :photo_name
+  attr_reader :qrcode_manager, :photo_manager, :photo_name, :asset
 
   def initialize( qrcode_manager, photo_manager, photo_name )
     @qrcode_manager = qrcode_manager
@@ -8,22 +8,23 @@ class AssetManager
   end
 
   def save!
-    asset = Asset.where( photo_name: self.photo_name ).first_or_initialize
+    @asset = Asset.where( photo_name: self.photo_name ).first_or_initialize
 
-    if asset.new_record?
-      asset.qrcode = self.qrcode_manager.qrcode_image.to_s
-      asset.qrcode.name = Asset::QRCODE_IMAGE_NAME
-      asset.token = self.qrcode_manager.token
-      asset.url = self.qrcode_manager.url
+    if @asset.new_record?
+      @asset.qrcode = self.qrcode_manager.qrcode_image.to_s
+      @asset.qrcode.name = Asset::QRCODE_IMAGE_NAME
+      @asset.token = self.qrcode_manager.token
+      @asset.url = self.qrcode_manager.url
     end
 
     if self.photo_manager.photo_image_exists?
-      asset.photo = self.photo_manager.photo_image
-      asset.photo.name = self.photo_name
+      @asset.photo = self.photo_manager.photo_image
+      @asset.photo.name = self.photo_name
     end
 
-    asset.save!
+    @asset.save!
 
     self.photo_manager.remove_photo_from_drop_folder
   end
 end
+
