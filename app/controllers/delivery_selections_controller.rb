68034ca6bc39343887a0fb40159aae04a7e1eb4b
Marc Schopper <ms@fournova.com>
2014-02-08 11:36:48 +0100

    Refactor in DeliverySelectionsController

---
+++ app/controllers/delivery_selections_controller.rb
@@ -0,0 +1,37 @@
+class DeliverySelectionsController < ApplicationController
+  before_filter :set_asset
+
+  def index
+  end
+
+  def create
+    email = params[:email]
+
+    if valid_email_and_not_blacklisted
+      UserMailer.send_photo( email, @asset ).deliver
+
+      @asset.increment_num_photo_downloads!
+
+      flash[:notice] = "Das Photo wurde soeben an #{email} versendet!"
+    elsif email_with_wrong_format
+
+      flash[:notice] = "Sorry, mit dieser E-Mail-Addresse können wir nichts anfangen"
+    end
+
+    render action: 'index'
+  end
+
+  def valid_email_and_not_blacklisted
+    email = params[:email]
+
+    ( email.present? and Devise.email_regexp.match( email ) and
+      BlacklistedEmail.where( email: email ).count.zero? )
+  end
+
+  def email_with_wrong_format
+    email = params[:email]
+
+    ( email.present? and not Devise.email_regexp.match( email ))
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Thomeli2Webapp::Application.routes.draw do
   get '/qrcode/image', to: 'qrcodes#image'
   get '/qrcode/url', to: 'qrcodes#url'
 
+  get '/:token', to: 'delivery_selections#index'
+  post '/:token', to: 'delivery_selections#create', as: :send_photo_in_email
+
+  root to: 'token#input_token', as: 'input_token'
+
   get '/download/:token', to: 'downloads#index'
-
-  get '/:token', to: 'tokens#index'
-  post '/:token', to: 'tokens#create'
-
-  resources :downloads, only: [:index, :create]
 end
